0e612251bcea9ec92ed5acdc2c0c94c7a698c1a5
Oliver Werner <oow@ruc.dk>
2024-03-22 10:47:08 +0100

    Tile coding works

--- algorithm/tile_code.py
+++ algorithm/tile_code.py
@@ -8,9 +8,11 @@ class TileCoder:
         self.state_space_ranges = state_space_ranges
         self.tile_size = (np.array(state_space_ranges[:, 1], dtype=float) - np.array(state_space_ranges[:, 0], dtype=float)) / num_tiles
 
-    def get_tile_index(self, sensor_index, sensor_value):
+    def get_tile_index(self, tiling, sensor_value):
         """Returns the tile index for the given sensor index and value."""
-        return int(np.floor(sensor_value / self.tile_size[sensor_index]))
+        offset = tiling * self.tile_size[tiling]
+        return int(np.floor(sensor_value / self.tile_size[tiling] + offset))
+
 
     def encode(self, state):
         """Takes a state and encodes it based on tile coding - Returns a Sensor object"""
@@ -21,7 +23,10 @@ class TileCoder:
             for sensor_reading in [hole_sensor, beams_sensor, key_sensor, lock_sensor]:
                 sensor_reading = (sensor_reading[tiling])
                 tiling_sensor_readings.append(self.get_tile_index(tiling, sensor_reading))
+                #print(f"SENSOR READING {sensor_reading}")
+                #print(self.get_tile_index(tiling, sensor_reading))
             encoded_sensors = Sensors(state.key_found, [tiling_sensor_readings[0]], [tiling_sensor_readings[1]], [tiling_sensor_readings[2]], [tiling_sensor_readings[3]])
+            #print(f"ENCODED STATE: {encoded_sensors}")
 
         return encoded_sensors
 
